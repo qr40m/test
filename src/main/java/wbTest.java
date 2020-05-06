177335e1b6d71765e30ae656d5c613039c6030ab
qr40m <Uran238_235>
2020-05-06 20:37:00 +0300

    another one

--- src/main/java/wbTest.java
+++ src/main/java/wbTest.java
@@ -1,16 +1,5 @@
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byXpath;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.openqa.selenium.firefox.ProfilesIni;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.springframework.web.client.RestTemplate;
 
 
 public class wbTest {
@@ -19,6 +8,7 @@ public class wbTest {
     public void wbTest() {
         //just some assertion
         assertEquals(1, 1);
+        assertEquals(1,1);
     }
 
 }
